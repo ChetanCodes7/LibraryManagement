5101bb1528ab57e6f224741ef1277ceacbc65085
ChetanCodes7 <timilsenachetan53@gmail.com>
2025-12-09 20:29:45 +0900

    updated

--- src/BorrowRecord.cpp
+++ src/BorrowRecord.cpp
@@ -1,6 +1,6 @@
 #include "BorrowRecord.hpp"    // Include the header file for the BorrowRecord class
 
-// Constructor for BorrowRecord; initializes itemId and memberName
+// Constructor for BorrowRecord ; initializes itemId and memberName
 BorrowRecord::BorrowRecord(int id, string member)
     : itemId(id), memberName(member) {}
 
